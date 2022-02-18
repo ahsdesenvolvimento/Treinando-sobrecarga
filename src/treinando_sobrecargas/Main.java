e2d2fbbc00a9f2cf9369827b1b8a271e608e59df
André Henrique Sousa <ahsdesenvolvimento@gmail.com>
2022-02-18 19:26:58 -0300

    first commit

---
+++ src/treinando_sobrecargas/Main.java
@@ -0,0 +1,19 @@
+package treinando_sobrecargas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Quadrilátero - Treinando sobrecargas
+		
+		
+        System.out.println("Exercício quadrilátero - sobrecarga de metodos\n\n");
+        Quadrilatero.area(7);
+        Quadrilatero.area(6d,6d);
+        Quadrilatero.area(5,6,7);
+        Quadrilatero.area(5f,5f);
+        
+	}
+
+}
